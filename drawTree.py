3f0bfe0b64733717a5af185a375139440b6870de
DominikStyp <dominik.stypula1@gmail.com>
2024-11-05 23:34:32 +0100

    refactoring

--- drawTree.py
+++ drawTree.py
@@ -4,14 +4,18 @@ endLine = ''
 for lineNr in range(1, tall+1):
     spaces = tall - lineNr
     line = ''
+
     for s in range(spaces):
         line += ' '
-    for leftHash in range(lineNr):
-        line += '#'
+
+    line += '#'
+
     for rightHash in range(lineNr - 1):
-        line += '#'
+        line += '##'
+
     if endLine == '':
         endLine = line
+
     print(line)
 
 print(endLine)
